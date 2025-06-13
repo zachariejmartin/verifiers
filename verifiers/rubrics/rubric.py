f28a799d5e3830f6150f0b284b401f6574fe0e78
William Brown <williambrown97@gmail.com>
2025-06-13 03:08:51 +0000

    stuff

--- verifiers/rubrics/rubric.py
+++ verifiers/rubrics/rubric.py
@@ -146,7 +146,6 @@ class Rubric:
             total=len(prompts),
             desc=f"Evaluating {len(prompts)} rollouts"
         )
-        print(rewards)
         return {k: [item[k] for item in rewards] for k in rewards[0]}
     
     def score_rollouts(self,
