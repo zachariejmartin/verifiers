e7bd57c5cd43e9586e00a7f63e7559375732dcd6
William Brown <williambrown97@gmail.com>
2025-07-10 21:28:17 +0000

    add '/v1' to judgerubric example

--- verifiers/examples/self_reward.py
+++ verifiers/examples/self_reward.py
@@ -2,9 +2,9 @@ import verifiers as vf
 from openai import OpenAI
 from datasets import load_dataset
 
-model_name = "Qwen/Qwen2.5-7B-Instruct" # lol
+model_name = "Qwen/Qwen2.5-7B-Instruct"
 judge_prompt = "Q: {question}\nA: {answer}\nGiven: {response}\nRespond with a score between 0.0 and 1.0."
-rubric = vf.JudgeRubric(client=OpenAI(base_url="http://localhost:8000"), model=model_name, judge_prompt=judge_prompt)
+rubric = vf.JudgeRubric(client=OpenAI(base_url="http://0.0.0.0:8000/v1"), model=model_name, judge_prompt=judge_prompt)
 vf_env = vf.SingleTurnEnv( 
     dataset=load_dataset("willcb/my-dataset", data_files="train"), # HF dataset with "question" and "answer" columns
     system_prompt="You are a helpful assistant.",
